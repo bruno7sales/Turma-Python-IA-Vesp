4c8f1a3285e53f51c2133fc08eb26329a766f104
bruno7sales <bruno07sales@gmail.com>
2024-08-07 15:31:16 -0300

    atualizando

---
+++ juntar/main.py
@@ -0,0 +1,12 @@
+# Lista de carros
+carros= ['Mitsubish', 'Ferrari', 'Lamborguine', 'BMW', 'Subaru']
+
+# Define separador
+separador = '-'
+
+# Variavel que armazena todos os itens da lista em um string
+lista_carro = separador.join(carros)
+
+# Exibe a nova string
+print(lista_carro)
+
